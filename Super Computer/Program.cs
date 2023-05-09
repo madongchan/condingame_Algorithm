d7b4c5f51960effc524dc5159b2c2e32fa76d984
마동찬 <mdc0078@gmail.com>
2023-05-09 17:55:34 +0900

    탐욕 알고리즘 문제 풀이 중
    돈을 공평하게 분배하는 문제는 완료
    
    지금은 연구기간이 안겹치는 가장 여군를 많이 할 수 있는 경우를 찾는 문제 풀이 중

---
+++ Super Computer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+//풀이 방법: 종료일이 가장 빠른걸 선택하고 기간이 중복되는건 제거한다
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        var periodLists = new List<(int startDay, int endDay)>(); // ValueTuple 자료구조
+        for (int i = 0; i < N; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int startDay = int.Parse(inputs[0]); // 연구 시작일
+            int duration = int.Parse(inputs[1]); // 연구 기간
+            int endDay = startDay + duration - 1; // 연구 종료일
+
+            periodLists.Add((startDay, endDay)); // 연구기간 리스트에 저장
+        }
+
+        foreach (var period in periodLists)
+        {
+
+        }
+        Console.WriteLine("answer");
+    }
+}
---
+++ The Gift/The Gift.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine()); // 선물 구매에 동참하는 사람 수
+        int C = int.Parse(Console.ReadLine()); // 선물 가격
+        int[] Budgets = new int[N]; // 예산을 담을 배열
+
+        for (int i = 0; i < N; i++)
+        {
+            Budgets[i] = int.Parse(Console.ReadLine()); //각각의 예산
+        }
+
+        //예산의 합이 선물 가격보다 적으면
+        if (Budgets.Sum() < C)
+        {
+            Console.WriteLine("IMPOSSIBLE");
+            return;
+        }
+
+        //가장 예산이 적은 사람부터 차례대로 확인해야 가장 공평하게 선물 가격을 지불함
+        Array.Sort(Budgets);
+
+        //예산의 합이 선물 가격보다 크면 계속 진행
+        for (int i = 0; i < Budgets.Length; i++)
+        {
+            // 각각의 사람이 내야할 선물의 평균 값
+            // 평균은 계속 업데이트 해줘야함
+            int averageCost = C / N;
+
+            //만약 평균 값보다 예산이 적다면
+            if (Budgets[i] < averageCost)
+            {
+                Console.WriteLine(Budgets[i]);
+                C -= Budgets[i]; // 예산 전체를 낸다
+            }
+            else
+            {
+                Console.WriteLine(averageCost);
+                C -= averageCost;//예산에서 평균값만 내기
+            }
+            //낸 사람은 제외한다.
+            N--;
+        }
+    }
+}
